eec1134b9c62b9e4004ec11af22de6b3b86067d0
root <root@DESKTOP-O9EH2P0>
2025-01-07 00:53:03 +0900

    名前編集可能

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,19 +1,34 @@
 class BirdsController < ApplicationController
-  def index
-    @bird = Bird.first_or_create(name: "ぴよちゃん", health: 100, growth_stage: "baby")
-  end
+    def index
+      @bird = Bird.first_or_create(name: "ぴよちゃん", health: 20, growth_stage: "baby")
+    end
 
-  def feed
-    @bird = Bird.first
-    new_health = [@bird.health + 5, 20].min # 最大値を20に制限
-    @bird.update(health: new_health)
-    render json: { health: new_health }
-  end
+    def feed
+      @bird = Bird.first
+      new_health = [@bird.health + 5, 20].min # 最大値を20に制限
+      @bird.update(health: new_health)
+      render json: { health: new_health }
+    end
 
-  def decrease_health
-    @bird = Bird.first
-    new_health = [@bird.health - 1, 0].max # 最小値を0に制限
-    @bird.update(health: new_health)
-    render json: { health: new_health }
+    def decrease_health
+      @bird = Bird.first
+      new_health = [@bird.health - 1, 0].max # 最小値を0に制限
+      @bird.update(health: new_health)
+      render json: { health: new_health }
+    end
+
+    # 名前編集画面を表示
+    def edit_name
+      @bird = Bird.first_or_create(name: "ぴよちゃん", health: 20, growth_stage: "baby")
+    end
+
+    def update_name
+      @bird = Bird.first
+      if @bird.update(name: params[:name]) # params[:bird][:name] を params[:name] に変更
+        redirect_to root_path, notice: "名前を更新しました！"
+      else
+        flash[:alert] = "名前の更新に失敗しました。"
+        render :edit_name
+      end
+    end
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get "birds/health_status", to: "birds#health_status"
   post "birds/decrease_health", to: "birds#decrease_health"
   post "birds/feed", to: "birds#feed" # 新しいエンドポイント
+
+  # 名前編集機能のルートを追加
+  get "birds/edit_name", to: "birds#edit_name", as: :edit_name_birds
+  patch "birds/update_name", to: "birds#update_name", as: :update_name_birds
 end
+
