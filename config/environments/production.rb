917d6011f2c4bb24f3aa65e779f762b192b3a207
root <root@DESKTOP-O9EH2P0>
2025-01-07 17:12:33 +0900

    WEBブログ参考にしたmatiga2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,10 +83,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
- # 追加
- config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
-
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
