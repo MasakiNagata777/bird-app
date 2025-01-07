55f751710bd6bde977b14778e55720dbe1f12ade
root <root@DESKTOP-O9EH2P0>
2025-01-07 16:53:43 +0900

    pumaいじっている2

--- config/puma.rb
+++ config/puma.rb
@@ -3,12 +3,12 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Bind to 0.0.0.0 and use PORT environment variable
+# Port to bind Puma to (Render recommends binding to 0.0.0.0)
 port ENV.fetch("PORT") { 3000 }
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 
-# Set the environment
-environment ENV.fetch("RAILS_ENV") { "development" }
+# Set the environment (default to production for Render)
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # PID and state file locations
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -22,3 +22,13 @@ preload_app!
 
 # Allow restart with `rails restart`
 plugin :tmp_restart
+
+# Before fork: This is used to disconnect the database connection before forking workers
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
+
+# On worker boot: Reconnect the database
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
